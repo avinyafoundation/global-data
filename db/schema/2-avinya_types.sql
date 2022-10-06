fa21df7a4855ae02f78129fddfaea354b153ed2a
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2022-10-06 19:30:37 +0530

    Update avinya types to latest spec

--- db/schema/2-avinya_types.sql
+++ db/schema/2-avinya_types.sql
@@ -4,31 +4,34 @@ CREATE TABLE IF NOT EXISTS avinya_type (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     active BOOLEAN NOT NULL,
     global_type ENUM(
-        "Employee",
-        "Customer",
-        "Volunteer",
-        "Applicant",
-        "Team"
+        "employee",
+        "consultant",
+        "customer",
+        "volunteer",
+        "applicant",
+        "team",
+        "advisor",
+        "director",
+        "donor"
     ) NOT NULL,
     name VARCHAR(255),
     foundation_type ENUM(
-        "Executive",
-        "Advisor",
-        "Educator",
-        "Technology",
-        "Operations",
-        "HR",
-        "Parent",
-        "Student"
+        "executive", -- Foundation Level
+        "academic", -- specifically academic instruction
+        "staff", -- l.t. (<) Foundation Level
+        "parent",
+        "student"
     ),
     focus ENUM(
-        "Foundation",
-        "Vocational-IT",
-        "Vocational-Healthcare",
-        "Vocational-Hospitality",
-        "Operations",
-        "HR",
-        "Technology"
+        "bootcamp",
+        "information-technology",
+        "healthcare",
+        "hospitality",
+        "operations",
+        "human-resources",
+        "technology",
+        "marketing",
+        "finance"
     ),
     level INT
 );
